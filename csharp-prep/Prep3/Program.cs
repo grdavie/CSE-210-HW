da53eff58ac7d6aa3e876c2b013f7096dc15622c
grdavie <gaylereynadavie@gmail.com>
2023-04-25 11:38:40 +1000

    stretch challenges completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,39 +5,60 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Prep 3: Guess the Magic Number\n");
+        string runAgain = "y";
 
-        //Console.Write("What is the magic number? ");
-        //string numberInput = Console.ReadLine(); 
-        //int magicNumber = int.Parse(numberInput);
-
-        Random randomGenerator = new Random();
-        int magicNumber = randomGenerator.Next(1, 100);
-
-        int guess = 0;
-
-        while (magicNumber != guess)
+        while (runAgain == "y")
         {
-            Console.Write("What is your guess? ");
-            string guessInput = Console.ReadLine();
-            guess = int.Parse(guessInput);
-            
-            if (guess > magicNumber)
+            //Console.Write("What is the magic number? ");
+            //string numberInput = Console.ReadLine(); 
+            //int magicNumber = int.Parse(numberInput);
+
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 100);
+
+            int guess = 0;
+            int guessCount = 1;
+
+            while (magicNumber != guess)
             {
-                Console.WriteLine("Lower");
-                
+
+                Console.Write("What is your guess? ");
+                string guessInput = Console.ReadLine();
+                guess = int.Parse(guessInput);
+
+                if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+
+                }
+
+               else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+
+
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+
+                guessCount = guessCount + 1;
+
             }
 
-            else if (guess < magicNumber)
+            if (guessCount == 1)
             {
-                Console.WriteLine("Higher");
+                Console.WriteLine($"It took you {guessCount} guess.");
             }
 
-        
             else
             {
-                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"It took you {guessCount} guesses.");
             }
 
+            Console.WriteLine("Would you like to keep going? (y/n) ");
+            runAgain = Console.ReadLine();
         }
     }
 }
