dd81038d6c5fcdf58781d4b497df4fbba0ee43c4
grdavie <gaylereynadavie@gmail.com>
2023-04-25 11:08:19 +1000

    step 1 complete

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Prep 3: Guess the Magic Number\n");
+
+        Console.Write("What is the magic number? ");
+        string numberInput = Console.ReadLine(); 
+
+        Console.Write("What is your guess? ");
+        string guessInput = Console.ReadLine();
+
+        int magicNumber = int.Parse(numberInput);
+        int guess = int.Parse(guessInput);
+
+        if (guess > magicNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+
+        else if (guess < magicNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
+
+
     }
 }
