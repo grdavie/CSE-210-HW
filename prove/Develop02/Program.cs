26fb29319e5c1505ee7ee198f23f61fec490e0d5
grdavie <gaylereynadavie@gmail.com>
2023-05-08 07:33:46 +1000

    commiting so i can rethink my life decisions

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Collections.Generic;
+
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop02 World!");
+        
+        PromptGenerator prompt = new PromptGenerator();
+        prompt.ReturnRandomPrompt(prompt._prompt);
+    
+
     }
 }
---
+++ prove/Develop02/PromptGenerator.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+
+public class PromptGenerator
+{
+
+    public static List<string> ReadfromFile()
+    {
+        List<string> prompts = new List<string>();
+        string filename = "prompts.txt";
+
+        string[] lines = System.IO.File.ReadAllLines(filename);
+
+        foreach (string line in lines)
+        {
+            prompts.Add(line);
+        }
+
+        return prompts;
+
+    }
+
+    public List<string> _prompt = ReadfromFile();
+
+    public string ReturnRandomPrompt(List<string> _prompt)
+    {
+        Random randNum = new Random();
+        int randIndex = randNum.Next(_prompt.Count);
+
+        string randomPrompt = _prompt[randIndex];
+
+        return randomPrompt;
+    }
+}
