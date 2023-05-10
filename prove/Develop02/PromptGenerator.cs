4800fd7952af1a5cef2709adc8453adbcced60f2
grdavie <gaylereynadavie@gmail.com>
2023-05-10 20:45:20 +1000

    notes added bcoz i finally understand what happens

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 
 public class PromptGenerator
 {
-    public List<string> _prompt = ReadfromFile();
+    public List<string> _prompt = ReadfromFile(); //the list that was returned by the ReadFromFile() method
+    
 
-    public static List<string> ReadfromFile()
+    public static List<string> ReadfromFile() //reads the prompts from the text file and returns it as a list
     {
         List<string> prompts = new List<string>();
         string filename = "prompts.txt";
@@ -22,12 +23,12 @@ public class PromptGenerator
     }
 
     
-    public string ReturnRandomPrompt(List<string> _prompt)
+    public string ReturnRandomPrompt(List<string> _prompt) //takes in the list of prompts as its parameter to return a random prompt string
     {
         Random randNum = new Random();
-        int randIndex = randNum.Next(_prompt.Count);
+        int randIndex = randNum.Next(_prompt.Count); //returns a non-negative random integer less than the maximum to account for 0 index
 
-        string randomPrompt = _prompt[randIndex];
+        string randomPrompt = _prompt[randIndex]; //prompt randomly selected from the list of prompts using the random index generated
 
         return randomPrompt;
     }
