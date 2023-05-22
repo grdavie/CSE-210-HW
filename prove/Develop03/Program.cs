2ba1bb30a3d9636141381e4e2e2c685dbd645736
grdavie <gaylereynadavie@gmail.com>
2023-05-22 23:07:05 +1000

    word class seems to be functioning correctly

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Console.WriteLine("--------------------------------------------");
+        Console.WriteLine("Welcome to the Scripture Memoriser Program");
+        Console.WriteLine("--------------------------------------------\n");
+
+        //reference class testing
+        Reference r1 = new Reference("John", "3", "16"); //constructor 1
+        Console.WriteLine(r1.GetReferenceString());
+        Reference r2 = new Reference("Proverbs", "3", "5", "6"); //constructor 2
+        Console.WriteLine(r2.GetReferenceString());
+
+        //word class testing
+        Word w1 = new Word("Hello");
+        w1.Show();
+        Console.WriteLine(w1.GetRenderedText());
+        w1.Hide();
+        Console.WriteLine(w1.GetRenderedText());
+
+        Word w2 = new Word("The");
+        w2.Hide();
+        Console.WriteLine(w2.GetRenderedText());
+        w2.Show();
+        Console.WriteLine(w2.GetRenderedText());
+
     }
 }
---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,52 @@
+using System;
+
+public class Word 
+{
+    private string _word;
+    private bool _hidden;
+
+    public Word (string word)
+    {
+        _word = word;
+    }
+
+    public void Hide()
+    {
+        _hidden = true;
+    }
+
+    public void Show()
+    {
+        _hidden = false;
+    }
+
+    public bool IsHidden()
+    {
+        return _hidden;
+    }
+
+    public string GetRenderedText()
+    {
+        if (_hidden == false) //if visibility is not hidden (false) return word text
+        {
+            return _word;
+        }
+
+        else //if visibility is hidden (true) return hidden word or underscores
+        {
+            
+            string hiddenWord = ""; //empty string
+
+            foreach (char c in _word) //iterate through the characters in the word string
+
+            {
+                hiddenWord += "_"; //for every character or letter, append an "_" to the hiddenWord string
+            }
+
+            return hiddenWord; //hiddenWord should now be a string of underscores equal to the word count
+
+
+        }
+
+    }
+}
