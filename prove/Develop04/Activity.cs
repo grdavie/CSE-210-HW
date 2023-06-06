14c27303cd5733f6daba806a9121ae958d223a05
grdavie <gaylereynadavie@gmail.com>
2023-06-06 22:05:20 +1000

    baby steps

---
+++ prove/Develop04/Activity.cs
@@ -0,0 +1,45 @@
+using System;
+
+public class Activity
+{
+    protected string _activityName;
+    protected string _description;
+    protected int _duration;
+
+    public Activity()
+    {
+
+    }
+
+}
+
+
+
+//Breathing Activity
+//This activity will help you relax by walking you through breathing in and out slowly. Clear your mind and focus on your breathing
+//Breathe in...(countdown from 4)
+//Now breathe out...(countdown from 6)
+
+//Reflecting Activity
+//This activity will help you refelct on the times in your life when you have shown strength and resilience. This will help you recognise the power you have and how you can use it in other aspects of your life. 
+//Consider the following prompt:
+// --- Random Prompt ---
+//When you have something in mind, press enter to continue. (consolekey enter?)
+
+//Now ponder on each of the following questions as they relate to this experience.
+//You may begin in: (countdown from 5)
+// > Random Question #1? (5 second spinner etc)
+// > Random Questions #2? (5 second spinner etc)
+// stretch - ensure you get no duplicate questions before you can restart them
+
+
+//Listing Activity
+//This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area. 
+//List as many responses as you can to the following prompt:
+// --- Random Question Prompt ---
+//You may begin in: (countdown from 5)
+// > {console readline} *add to a list of answers
+// > repeat until you run out of time
+// at the end of the timer display: You listed {#} items!
+
+//clear console to keep the screen clean each time an activity finishes or a new menu loads
