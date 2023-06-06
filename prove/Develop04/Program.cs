81c1e16c7c2aa0d56e8c8a580d7259d74502950b
grdavie <gaylereynadavie@gmail.com>
2023-06-06 22:30:20 +1000

    program menu

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        
+        int userChoice = -1;
+        
+        Console.Clear();
+
+        Console.WriteLine("--------------------------------------------");
+        Console.WriteLine("Welcome to the Mindfulness Program");
+        Console.WriteLine("--------------------------------------------\n");
+
+        DisplayMenu();
+        userChoice = GetUserChoice();
+
+        Console.WriteLine(userChoice);
+
+
+        static void DisplayMenu()
+        {
+            Console.WriteLine("Select an Activity from the Menu:\n");
+            Console.WriteLine("   1. Start breathing activity");
+            Console.WriteLine("   2. Start reflecting activity");
+            Console.WriteLine("   3. Start listing activity");
+            Console.WriteLine("   4. Quit");
+
+        }
+
+        static int GetUserChoice()
+        {
+            Console.Write("\nWhat would you like to do? ");
+            int userInput = int.Parse(Console.ReadLine());
+
+            return userInput;
+
+        }
     }
 }
