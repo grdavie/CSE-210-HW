c690a53f4085e62084b6bdab16aef4ab6708a3b5
grdavie <gaylereynadavie@gmail.com>
2023-07-07 07:43:21 +1000

    we're getting somewhere

---
+++ final/FinalProject/Investment.cs
@@ -0,0 +1,30 @@
+using System;
+
+
+public class Investment : Loan
+{
+    public Investment(int securityValue, int depositAmount, int loanTerm)
+    : base(securityValue, depositAmount, loanTerm)
+    {
+        //same constructor as base class
+    }
+
+    public override void SetInterestRate()
+    {
+        if(_lvr > 0.70 && _lvr <= 0.80) //lvr between 71-80 
+        {
+            _interestRate = 0.0659; //6.59% rate
+        }
+
+        else if(_lvr > 0.60 && _lvr <= 0.70) //lvr between 61-70
+        {
+            _interestRate = 0.0629; //6.29% rate
+        }
+
+        else if(_lvr <= 0.60) //LVR 60 and under
+        {
+            _interestRate = 0.0619; //6.19% rate
+        }
+    }
+
+}
---
+++ final/FinalProject/OwnerOccupied.cs
@@ -0,0 +1,29 @@
+using System; 
+
+public class OwnerOccupied : Loan 
+{
+    public OwnerOccupied(int securityValue, int depositAmount, int loanTerm)
+    : base(securityValue, depositAmount, loanTerm)
+    {
+        //same constructor as base class
+    }
+    public override void SetInterestRate()
+    {
+        if(_lvr > 0.70 && _lvr <= 0.80) //lvr between 71-80 
+        {
+            _interestRate = 0.0634; //6.34% rate
+        }
+
+        else if(_lvr > 0.60 && _lvr <= 0.70) //lvr between 61-70
+        {
+            _interestRate = 0.0604; //6.04% rate
+        }
+
+        else if(_lvr <= 0.60) //LVR 60 and under
+        {
+            _interestRate = 0.0594; //5.94% rate
+        }
+    }
+
+
+}
